8381031055b697f30907a3700468c5934d1cecad
Ani <aninozaridze@gmail.com>
2020-04-12 17:08:12 +0400

    add check questions activity

--- app/src/main/java/com/example/homework1/CheckActivity.java
+++ app/src/main/java/com/example/homework1/CheckActivity.java
@@ -1,7 +1,21 @@
 package com.example.homework1;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CheckActivity extends AppCompatActivity {
+import com.example.homework1.data.StorageSharePreference;
 
+public class CheckActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answers);
+        TextView textView = findViewById(R.id.answers);
+        Intent intent = getIntent();
+        textView.setText(intent.getStringExtra("answers").toString());
+    }
 }
