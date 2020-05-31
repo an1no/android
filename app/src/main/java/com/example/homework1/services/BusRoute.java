e3a7c9bf7d1af4794ee443e56ecdd371a9d4a825
Ani <aninozaridze@gmail.com>
2020-05-31 23:38:35 +0400

    unfinished

---
+++ app/src/main/java/com/example/homework1/services/BusRoute.java
@@ -0,0 +1,13 @@
+package com.example.homework1.services;
+
+public class BusRoute {
+    private String routeNumber;
+
+    public String getRouteNumber() {
+        return routeNumber;
+    }
+
+    public void setRouteNumber(String routeNumber) {
+        this.routeNumber = routeNumber;
+    }
+}
