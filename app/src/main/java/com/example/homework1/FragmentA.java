171acab6e760e86b371dcc2c6318df0bc81839a5
Ani <aninozaridze@gmail.com>
2020-06-02 16:04:21 +0400

    add arrayAdapters

--- app/src/main/java/com/example/homework1/FragmentA.java
+++ app/src/main/java/com/example/homework1/FragmentA.java
@@ -10,11 +10,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.view.View.OnClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
@@ -24,6 +29,8 @@ import com.example.homework1.classes.User;
 import com.example.homework1.data.Storage;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentA extends Fragment {
     public static String NOTIFICATION = "com.example.homework1.NOTIFICATION";
@@ -34,10 +41,26 @@ public class FragmentA extends Fragment {
     private User zezva = new User("zezva");
     private Message message;
     private Messages messages;
+    Storage storage;
+    private ListView listView;
+    private MessageArrayAdapter messageArrayAdapter;
+    private ArrayList<Message> messageArrayList;
     @Override
     public View onCreateView( final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.view_fragment_a, container, false);
         editText = view.findViewById(R.id.input);
+        listView = view.findViewById(R.id.chat1);
+        messageArrayAdapter = new MessageArrayAdapter(getContext(), 0,new ArrayList<Message>());
+        storage = new Storage();
+        Object ChatStorage = storage.getObject(getContext(), "chat_storage", Messages.class);
+        if(ChatStorage!=null){
+            messages =(Messages) ChatStorage;
+            messageArrayList = messages.getMessages();
+            messageArrayAdapter.addAll(messageArrayList);
+        } else {
+            messages = new Messages();
+        }
+        listView.setAdapter(messageArrayAdapter);
         view.findViewById(R.id.send1).setOnClickListener(new OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
@@ -47,13 +70,6 @@ public class FragmentA extends Fragment {
                 message.setMessage(notificationData);
                 message.setUser(zezva);
                 message.setDate(LocalDateTime.now().toString());
-                Storage storage = new Storage();
-               Object ChatStorage = storage.getObject(getContext(), "chat_storage", Messages.class);
-               if(ChatStorage!=null){
-                   messages =(Messages) ChatStorage;
-               } else {
-                   messages = new Messages();
-               }
                 messages.addMessages(message);
                 storage.add(getContext(),"chat_storage", messages);
                 Intent intent = new Intent();
@@ -80,6 +96,33 @@ public class FragmentA extends Fragment {
                 String dataText = intent.getStringExtra(FragmentB.NOTIFICATION_DATA);
                 Toast.makeText(context, dataText, Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+    class MessageArrayAdapter extends ArrayAdapter<Message> {
+        private Context mContext;
+
+        public MessageArrayAdapter(Context context,
+                                   int resource,
+                                   List<Message> messages){
+            super(context,resource,messages);
+            mContext = context;
+        }
+
+        @NonNull
+        @Override
+        public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            final Message message = getItem(position);
+            LayoutInflater inflater = (LayoutInflater)mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(R.layout.view_chat, parent,false);
+            TextView textView1 = view.findViewById(R.id.zezva);
+            TextView textView2 = view.findViewById(R.id.mzia);
+            if(message.getUser().getName().equals("mzia")) {
+                textView2.setText(message.getMessage());
+            } else {
+                textView1.setText(message.getMessage());
+            }
+            return  view;
         }
     }
 }
--- app/src/main/java/com/example/homework1/FragmentB.java
+++ app/src/main/java/com/example/homework1/FragmentB.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
 
@@ -24,7 +28,12 @@ import com.example.homework1.classes.Messages;
 import com.example.homework1.classes.User;
 import com.example.homework1.data.Storage;
 
+import org.w3c.dom.Text;
+
+import java.io.Console;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentB extends Fragment {
     public static String NOTIFICATION = "com.example.homework1.NOTIFICATION";
@@ -34,11 +43,26 @@ public class FragmentB extends Fragment {
     private Message message;
     private Messages messages;
     private EditText editText;
+    private ListView listView;
+    private MessageArrayAdapter messageArrayAdapter;
+    private ArrayList<Message> messageArrayList;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,   @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View view = inflater.inflate(R.layout.view_fragment_b, container, false);
+        listView = view.findViewById(R.id.chat2);
         editText = view.findViewById(R.id.input);
+        messageArrayAdapter = new MessageArrayAdapter(getContext(), 0,new ArrayList<Message>());
+        Storage storage = new Storage();
+        Object ChatStorage = storage.getObject(getContext(), "chat_storage", Messages.class);
+        if(ChatStorage!=null){
+            messages =(Messages) ChatStorage;
+            messageArrayList = messages.getMessages();
+            messageArrayAdapter.addAll(messageArrayList);
+        } else {
+            messages = new Messages();
+        }
+        listView.setAdapter(messageArrayAdapter);
         view.findViewById(R.id.send2).setOnClickListener(new OnClickListener() {
         @RequiresApi(api = Build.VERSION_CODES.O)
         @Override
@@ -50,11 +74,6 @@ public class FragmentB extends Fragment {
             message.setDate(LocalDateTime.now().toString());
             Storage storage = new Storage();
             Object ChatStorage = storage.getObject(getContext(), "chat_storage", Messages.class);
-            if(ChatStorage!=null){
-                messages =(Messages) ChatStorage;
-            } else {
-                messages = new Messages();
-            }
             messages.addMessages(message);
             storage.add(getContext(),"chat_storage", messages);
             Intent intent = new Intent();
@@ -81,8 +100,35 @@ public class FragmentB extends Fragment {
             if (intent.hasExtra(NOTIFICATION_DATA)) {
                 String dataText = intent.getStringExtra(NOTIFICATION_DATA);
                 Toast.makeText(context, dataText, Toast.LENGTH_SHORT).show();
-
             }
+        }
+    }
+
+    class MessageArrayAdapter extends ArrayAdapter<Message> {
+        private Context mContext;
+
+        public MessageArrayAdapter(Context context,
+                                    int resource,
+                                    List<Message> messages){
+            super(context,resource,messages);
+            mContext = context;
+        }
+
+        @NonNull
+        @Override
+        public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            final Message message = getItem(position);
+            LayoutInflater inflater = (LayoutInflater)mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(R.layout.view_chat, parent,false);
+            TextView textView1 = view.findViewById(R.id.zezva);
+            TextView textView2 = view.findViewById(R.id.mzia);
+            if(message.getUser().getName().equals("zezva")) {
+                textView2.setText(message.getMessage());
+            } else {
+                textView1.setText(message.getMessage());
+            }
+            return  view;
         }
     }
 }
